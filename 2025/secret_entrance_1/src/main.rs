4ac657b5106329e28e00c334dced5f2d0d4b6417
Dominic Davies <dominictdavies@gmail.com>
2025-12-18 02:20:23 +0800

    Add guess comparison

--- 2025/secret_entrance_1/src/main.rs
+++ 2025/secret_entrance_1/src/main.rs
@@ -1,8 +1,11 @@
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input a guess.");
+    println!("Please input your guess.");
 
     let mut guess = String::new();
 
@@ -10,5 +13,15 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}")
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
