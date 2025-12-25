a08076b9748f5e0c8f23e6e191e4e2849d956cb2
Dominic Davies <dominictdavies@gmail.com>
2025-12-25 23:13:26 +0800

    Store num_zeros in struct

--- 2025/secret_entrance_1/src/main.rs
+++ 2025/secret_entrance_1/src/main.rs
@@ -4,35 +4,57 @@ use std::fs;
 
 const DIAL_START: u8 = 50;
 
-struct Dial(u8);
+struct Dial {
+    pointing_at: u8,
+    num_zeros: u16,
+}
 
 impl Dial {
+    fn new(pointing_at: u8) -> Self {
+        Self {
+            pointing_at,
+            num_zeros: 0,
+        }
+    }
+
+    fn get_num_zeros(self) -> u16 {
+        self.num_zeros
+    }
+
     fn wrapping_sub(&mut self, rhs: u16) {
         let rhs: u8 = (rhs % 100) as u8;
 
-        if self.0.checked_sub(rhs).is_none() {
-            self.0 = (self.0 + 100) - rhs;
+        if self.pointing_at.checked_sub(rhs).is_none() {
+            self.pointing_at = (self.pointing_at + 100) - rhs;
         } else {
-            self.0 = self.0 - rhs;
+            self.pointing_at = self.pointing_at - rhs;
+        }
+
+        if self.pointing_at == 0 {
+            self.num_zeros += 1;
         }
     }
 
     fn wrapping_add(&mut self, rhs: u16) {
         let rhs: u8 = (rhs % 100) as u8;
 
-        self.0 = (self.0 + rhs) % 100;
+        self.pointing_at = (self.pointing_at + rhs) % 100;
+
+        if self.pointing_at == 0 {
+            self.num_zeros += 1;
+        }
     }
 }
 
 impl PartialEq<u8> for Dial {
     fn eq(&self, other: &u8) -> bool {
-        self.0 == *other
+        self.pointing_at == *other
     }
 }
 
 impl fmt::Display for Dial {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0)
+        write!(f, "{}", self.pointing_at)
     }
 }
 
@@ -40,8 +62,7 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Failed to read the file");
     let lines: Vec<&str> = input.split('\n').collect();
 
-    let mut dial = Dial(DIAL_START);
-    let mut zero_count: u16 = 0;
+    let mut dial = Dial::new(DIAL_START);
 
     for line in lines {
         if line == "" {
@@ -58,11 +79,8 @@ fn main() {
         } else {
             panic!("Direction was not 'L' or 'R'");
         }
-
-        if dial == 0 {
-            zero_count += 1;
-        }
     }
 
-    println!("zero_count: {zero_count}");
+    let num_zeros = dial.get_num_zeros();
+    println!("zero_count: {num_zeros}");
 }
