540f3eb7074a3779f70d7d805c6bdfcbbba6994d
Dominic Davies <dominictdavies@gmail.com>
2025-12-26 00:59:15 +0800

    Solved secret entrance

--- 2025/secret_entrance_1/src/main.rs
+++ 2025/secret_entrance_1/src/main.rs
@@ -2,8 +2,6 @@ use core::panic;
 use std::fmt;
 use std::fs;
 
-const DIAL_START: u8 = 50;
-
 struct Dial {
     pointing_at: u8,
     num_zeros: u16,
@@ -17,32 +15,32 @@ impl Dial {
         }
     }
 
-    fn get_num_zeros(self) -> u16 {
+    fn get_num_zeros(&self) -> u16 {
         self.num_zeros
     }
 
     fn wrapping_sub(&mut self, rhs: u16) {
-        let rhs: u8 = (rhs % 100) as u8;
+        self.num_zeros += rhs / 100;
+        let rhs = (rhs % 100) as u8;
 
-        if self.pointing_at.checked_sub(rhs).is_none() {
-            self.pointing_at = (self.pointing_at + 100) - rhs;
-        } else {
-            self.pointing_at = self.pointing_at - rhs;
-        }
-
-        if self.pointing_at == 0 {
+        let result = (self.pointing_at + 100) - rhs;
+        if result <= 100 && self.pointing_at > 0 {
             self.num_zeros += 1;
         }
+
+        self.pointing_at = result % 100;
     }
 
     fn wrapping_add(&mut self, rhs: u16) {
-        let rhs: u8 = (rhs % 100) as u8;
+        self.num_zeros += rhs / 100;
+        let rhs = (rhs % 100) as u8;
 
-        self.pointing_at = (self.pointing_at + rhs) % 100;
-
-        if self.pointing_at == 0 {
+        let result = self.pointing_at + rhs;
+        if result >= 100 {
             self.num_zeros += 1;
         }
+
+        self.pointing_at = result % 100;
     }
 }
 
@@ -62,7 +60,7 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Failed to read the file");
     let lines: Vec<&str> = input.split('\n').collect();
 
-    let mut dial = Dial::new(DIAL_START);
+    let mut dial = Dial::new(50);
 
     for line in lines {
         if line == "" {
@@ -72,15 +70,13 @@ fn main() {
         let (direction, distance) = line.split_at(1);
         let distance: u16 = distance.parse().expect("Failed to read the distance");
 
-        if direction == "L" {
-            dial.wrapping_sub(distance);
-        } else if direction == "R" {
-            dial.wrapping_add(distance);
-        } else {
-            panic!("Direction was not 'L' or 'R'");
+        match direction {
+            "L" => dial.wrapping_sub(distance),
+            "R" => dial.wrapping_add(distance),
+            _ => panic!("Direction was not L or R"),
         }
     }
 
     let num_zeros = dial.get_num_zeros();
-    println!("zero_count: {num_zeros}");
+    println!("num_zeros: {num_zeros}");
 }
